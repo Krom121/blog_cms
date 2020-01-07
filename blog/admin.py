75f8a1b435c144b6f2da74319c4e91d749938722
stephen <stephenmacgregor9@gmail.com>
2020-01-07 11:21:15 +0000

    REGISTERED MODELS

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Subscriber,Post,Category,Comment,NewContact,Author,AboutPage
 
-# Register your models here.
+admin.site.register(Subscriber)
+admin.site.register(Post)
+admin.site.register(Category)
+admin.site.register(Comment)
+admin.site.register(NewContact)
+admin.site.register(Author)
+admin.site.register(AboutPage)
